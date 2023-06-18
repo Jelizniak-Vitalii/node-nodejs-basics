560250d8f5fd4ab5cfae8757d2120a2f51590f1d
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2023-06-18 17:39:46 +0300

    feat: add rename file function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  try {
+    await fs.promises.rename(`${__dirname}/files/wrongFilename.txt`, `${__dirname}/files/properFilename.md`);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
