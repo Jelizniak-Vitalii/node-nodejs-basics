2290fb69e3026aa7fc30d0790e1228fc675d3e90
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2023-06-18 17:55:11 +0300

    feat: add read file function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  try {
+    const fileData = await fs.promises.readFile(`${__dirname}/files/fileToRead.txt`, 'utf-8');
+    console.log(fileData);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
